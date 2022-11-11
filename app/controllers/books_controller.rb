8c0e45e119f00e0f7a21445a0e054c6f6b22014d
maacho52 <117494270+maacho52@users.noreply.github.com>
2022-11-11 13:03:12 +0000

    [Update]createアクションのflashの場所を移動

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,13 +10,12 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[ :notice] = "Book was succesfully created."
       redirect_to book_path(@book.id)
     else
       render :index
     end
 
-    flash[ :notice] = "Book was successfully created."
-    redirect_to "/books/:id"
   end
 
   def show
