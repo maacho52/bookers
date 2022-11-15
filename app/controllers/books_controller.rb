3cfc163bc53ad0e8f0c825e5e2a961ce09443eff
maacho52 <117494270+maacho52@users.noreply.github.com>
2022-11-15 12:24:13 +0000

    [Update]bookを@bookに

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      flash[ :notice] = "Book was succesfully created."
+      flash[ :notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -28,12 +28,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      render :show
+      render :edit
     end
   end
 
